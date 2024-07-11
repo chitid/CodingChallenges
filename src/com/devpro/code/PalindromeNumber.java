281c72846d26c82dc2eaa307caf92df073a43924
dhana <dhana.devpro@gmail.com>
2024-07-11 11:55:03 -0500

    Coding practice

--- src/com/devpro/code/PalindromeNumber.java
+++ src/com/devpro/code/PalindromeNumber.java
@@ -1,4 +1,27 @@
 package com.devpro.code;
 
 public class PalindromeNumber {
+
+
+    public static void main(String[] args) {
+
+        int num=131;
+        int reverseNum=0;
+        int remainder=0;
+
+       while (num !=0){
+           remainder = num%10;
+           reverseNum = reverseNum*10+remainder;
+           num=num/10;
+       }
+
+       if(num==reverseNum){
+           System.out.println("Palindrome");
+       }
+
+
+
+    }
+
+
 }
