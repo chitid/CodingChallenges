6bb8d01f39edfb947d847f59b186d1dce52939e3
dhana <dhana.devpro@gmail.com>
2025-07-10 11:55:03 -0500

    Coding practice

--- src/com/devpro/code/ValidParenthes.java
+++ src/com/devpro/code/ValidParenthes.java
@@ -1,4 +1,37 @@
 package com.devpro.code;
 
-public class ValidParenthes {
+import java.util.Arrays;
+import java.util.Stack;
+
+public class ValidParenthes{
+
+    public static void main(String[] args) {
+
+        String str="()[]{}{}";
+
+        Stack<Character> stack=new Stack<>();
+
+       char []chars= str.toCharArray();
+
+       for(int i=0;i<chars.length;i++){
+           if(chars[i]=='(' || chars[i]=='{' || chars[i]=='['){
+               stack.push(chars[i]);
+           }
+           else if(chars[i]==')' || chars[i]=='}' || chars[i]==']'){
+               if(stack.isEmpty())
+                   return;
+               else {
+                   stack.pop();
+               }
+           }
+       }
+
+      if(stack.isEmpty()){
+          System.out.println(true);
+      } else {
+          System.out.println(false);
+      }
+
+
+    }
 }
